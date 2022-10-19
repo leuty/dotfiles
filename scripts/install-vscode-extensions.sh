8111775592a1c1f3cec44b25e0740f6a3ffba4c6
David Leutwyler <14977216+leuty@users.noreply.github.com>
2022-10-19 11:36:39 +0200

    Update install-vscode-extensions.sh

--- scripts/install-vscode-extensions.sh
+++ scripts/install-vscode-extensions.sh
@@ -2,9 +2,11 @@
 #!/bin/bash
 # Visual Studio Code :: Package list
 pkglist=(
+ms-python.black-formatter
 sourcery.sourcery
 )
 
 for i in ${pkglist[@]}; do
+  echo $i
   code --install-extension $i
 done
